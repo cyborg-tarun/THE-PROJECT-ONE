1bc7cc4c49df8ed9af6610b77dbbd3d2871908cb
tarundeep singh <64502281+cyborg-tarun@users.noreply.github.com>
2021-12-03 14:22:49 +0530

    tarun
    new add on , with sound

--- locker.py
+++ locker.py
@@ -2,8 +2,50 @@ import base64
 import os
 import time
 import sys
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
 
-password = "password"  
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+
+
+
+
+password = "open"  
 encode = base64.b64encode(password.encode("utf-8"))
 
 def goto(linenum):
@@ -12,11 +54,13 @@ def goto(linenum):
 
 line = 1
 while True:
+    speak('Enter your password for Lock or Unlock your folder:')
+    query = takeCommand().lower()
     pw = str(input("Enter your password for Lock or Unlock your folder: "))
 
     if pw == base64.b64decode(encode).decode("utf-8")  :
     # Change Dir Path where you have Locker Folder
-        os.chdir('''C:\\Users\\cybor\\Desktop\\THE PROJECT ONE''')
+        os.chdir('''C:\\Users\\91989\\OneDrive\\Desktop\\the project one\\THE-PROJECT-ONE-main''')
     # If Locker folder or Recycle bin does not exist then we will be create Locker Folder 
         if not os.path.exists("Locker"):
             if not os.path.exists("Locker.{645ff040-5081-101b-9f08-00aa002f954e}"):

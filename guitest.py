0e1fd8b316fe25e7c716c0731771b12f212359a6
saicoooo <82467348+saicoooo@users.noreply.github.com>
2021-12-04 13:15:09 +0530

    Update guitest.py

--- guitest.py
+++ guitest.py
@@ -1,5 +1,10 @@
 import sqlite3, hashlib
 from tkinter import *
+import base64
+import os
+import time
+import sys
+
 
 window = Tk()
 
@@ -9,13 +14,58 @@ window.title("vaultl gui")
 def loginscreen():
     window.geometry("350x300")
 
-    lbl=Label(window, text="vault login")
+    lbl=Label(window, text="vault login enter password")
     lbl.config(anchor=CENTER)
     lbl.pack()
-    Text= Entry(window,width="20")
+
+    Text= Entry(window,width="20", show="*")
     Text.pack()
-    btn= Button(window,text="submit", command="checkpassword")
+    Text.focus()
+
+    lbl1 = Label(window)
+    lbl1.pack()
+
+    def checkpassword():
+        password= "testpw"
+        if password == Text.get():
+           passwordVault()
+           folderlock()
+        else:
+            lbl1.config(text="wrong password")    
+
+
+    btn= Button(window,text="submit", command=checkpassword)
     btn.pack(pady=20)
+def passwordVault():    
+    for widget in window.winfo_children():
+        widget.destroy()
+    window.geometry("768x360")    
+
+    lbl= Label(window, text="Vault opened")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+
+def folderlock():
+
+    os.chdir('''D:\\private project''')
+    if not os.path.exists("Locker"):
+        if not os.path.exists("Locker.{645ff040-5081-101b-9f08-00aa002f954e}"):
+            os.mkdir("Locker")
+        else:
+            os.popen('attrib -h Locker.{645ff040-5081-101b-9f08-00aa002f954e}')
+            os.rename("Locker.{645ff040-5081-101b-9f08-00aa002f954e}","Locker")
+            sys.exit()
+            
+
+    else:
+        os.rename("Locker","Locker.{645ff040-5081-101b-9f08-00aa002f954e}")
+        os.popen('attrib +h Locker.{645ff040-5081-101b-9f08-00aa002f954e}')
+        sys.exit()
+        
+
+    
+
 
 loginscreen()    
 
